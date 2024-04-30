3a66ffd2ae611a499b8c6ac4170d050d4628ddc3
Alexander Fedorovskyi <kikban030@gmail.com>
2024-04-30 12:52:31 +0300

    feat: number of threads updated

--- run_bench.sh
+++ run_bench.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/sh
 
+procs=8
+
 printf "Run Benchmarks:\n\n"
+echo Number of processes: $procs
+
 
 echo "100 * 10 @ 10 * 100: "
 ./ublas ../resources/a100x10.txt ../resources/b10x100.txt
 ./sequential ../resources/a100x10.txt ../resources/b10x100.txt
 ./openmp ../resources/a100x10.txt ../resources/b10x100.txt
-mpiexec -n 12 ./openmpi ../resources/a100x10.txt ../resources/b10x100.txt
+mpiexec -n $procs ./openmpi ../resources/a100x10.txt ../resources/b10x100.txt
 printf "\n\n"
 
 
@@ -14,20 +18,20 @@ echo "100 * 100 @ 100 * 100: "
 ./ublas ../resources/a100x100.txt ../resources/b100x100.txt
 ./sequential ../resources/a100x100.txt ../resources/b100x100.txt
 ./openmp ../resources/a100x100.txt ../resources/b100x100.txt
-mpiexec -n 12 ./openmpi ../resources/a100x100.txt ../resources/b100x100.txt
+mpiexec -n $procs ./openmpi ../resources/a100x100.txt ../resources/b100x100.txt
 printf "\n\n"
 
 echo "1000 * 100 @ 100 * 1000: "
 ./ublas ../resources/a1000x100.txt ../resources/b100x1000.txt
 ./sequential ../resources/a1000x100.txt ../resources/b100x1000.txt
 ./openmp ../resources/a1000x100.txt ../resources/b100x1000.txt
-mpiexec -n 12 ./openmpi ../resources/a1000x100.txt ../resources/b100x1000.txt
+mpiexec -n $procs ./openmpi ../resources/a1000x100.txt ../resources/b100x1000.txt
 printf "\n\n"
 
 echo "1000 * 1000 @ 1000 * 1000: "
 ./ublas ../resources/a1000x1000.txt ../resources/b1000x1000.txt
 ./sequential ../resources/a1000x1000.txt ../resources/b1000x1000.txt
 ./openmp ../resources/a1000x1000.txt ../resources/b1000x1000.txt
-mpiexec -n 12 ./openmpi ../resources/a1000x1000.txt ../resources/b1000x1000.txt
+mpiexec -n $procs ./openmpi ../resources/a1000x1000.txt ../resources/b1000x1000.txt
 printf "\n\n"
 
